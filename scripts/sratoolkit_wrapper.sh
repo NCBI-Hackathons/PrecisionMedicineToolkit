decb6ab7d7da52ae1c91c736625baeb4925b2ae4
Ubuntu <ubuntu@ip-172-16-243-191.ec2.internal>
2018-03-20 21:20:41 +0000

    Attempt to update

--- scripts/sratoolkit_wrapper.sh
+++ scripts/sratoolkit_wrapper.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
 
+function usage {
+	echo "usage:  $programname -s data/example_SRR_list.txt -r reference_genome.fasta"
+	echo ""
+	echo "  -s example_SRR_list.txt 	List of SRR values to extract - always required."
+	echo "  -r reference_genome.fasta	Reference Genome for using bowtie2 to index - always required."
+	echo ""
+	documentTaxList
+
+}
+
+while getopts ":s:r:" o; do
+case "${s}" in 
+s)
+SRR_LIST=${OPTARG}
+;;
+r)
+REFERENCE=${OPTARG}
+;;
+:)
+echo "Invalid option: $OPTARG requires an argument"
+usage
+exit 0
+;;
+esac
+done
+
+mkdir SRR
+cd SRR
+
 # Generate the  bash scripts
 # Perform prefetch to get the SRA data
-python scripts/generateBatchScripts.prefetch.py data/example_SRRlist.txt
+while read SRR_LIST; do
+prefetch -L debug -t fasp -v -v $SRR_LIST
+done 
 
-# Convert the SRA into fastq
-python scripts/generateBatchScripts.fastq-dump.py data/example_SRRlist.txt
+while read SRR_LIST; do
+vdb-validate $SRR_LIST.sra &> $SRR_LIST.validation_out
+done
 
-# Align fastq to reference index
+while read SRR_LIST; do
+fastq-dump -v --gzip --split-files -O fastq_files/$SRR_LIST $SRR_List.sra
+done
 
+# Create reference index
