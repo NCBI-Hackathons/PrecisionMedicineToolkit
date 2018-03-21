81ca81294c30d41893b1c704a638c169504ac8ce
lulizou <luli.zou@gmail.com>
2018-03-21 12:04:37 -0400

    added gwas2json

--- scripts/gwas2json.py
+++ scripts/gwas2json.py
@@ -2,24 +2,57 @@ import os
 import sys
 import argparse
 import json
+import math
 import pandas as pd
 
 def gwas2json(gwasfile, outputdir):
     """Function to grab entries from GWAS file and spit out FHIR-compliant JSON
     in outputdir. Currently I am making it output (a) ResearchStudy file(s)."""
-    gwas = pd.read_table('results.txt', header = None)
+    gwas = pd.read_table('results.txt', header = 0)
     for index, row in gwas.iterrows():
-        title = row['6']
-        relatedArtifact_display = row['5']
-        disease = row['7']
+        filename = 'gwas_result{0}.json'.format(index)
+        title = row['STUDY']
+        url = row['LINK']
+        disease = row['DISEASE/TRAIT']
+        if type(row['INITIAL SAMPLE SIZE']) is str:
+            initial_sample_size = row['INITIAL SAMPLE SIZE']
+        else:
+            initial_sample_size = 'No initial sample size provided'
+        if type(row['REPLICATION SAMPLE SIZE']) is str:
+            replication_sample_size = row['REPLICATION SAMPLE SIZE']
+        else:
+            replication_sample_size = 'No replication sample size provided'
+        sample_info = initial_sample_size + replication_sample_size
+        gene = row['REPORTED GENE(S)']
+        rsid = row['SNPS']
+        var_type = row['CONTEXT']
+        write_ResearchStudy(filename = filename, title = title, url = url,
+                            disease = disease, sample_info = sample_info,
+                            gene = gene, rsID = rsid, var_type = var_type,
+                            outputdir = outputdir)
 
-
-def write_ResearchStudy(title, url, disease, sample_info, gene, rsID, var_type):
-
-
-
-
-
+def write_ResearchStudy(filename, title, url, disease, sample_info, gene, rsID,
+                        var_type, outputdir):
+    """See format here: https://www.hl7.org/fhir/researchstudy.html"""
+    with open(filename, 'w') as outfile:
+        json.dump({"resourceType" : "ResearchStudy",
+                   "title" : title,
+                   "status" : "completed",
+                   "category" : [{
+                           "text" : "gwas"
+                       }],
+                   "relatedArtifact" : [{
+                           "type" : "citation",
+                           "display" : title,
+                           "url" : url
+                       }],
+                   "description" : ("1. **Disease**: " + disease + "  " +
+                                   "2. **Sample Initial/Replicate**: " +
+                                   sample_info + "  " +
+                                   "3. **Gene**: " + gene + "  " +
+                                   "4. **rsID**: " + rsID + "  " +
+                                   "5. **Variant Type**: " + var_type)
+                   }, outfile)
 
 class App(object):
 
@@ -53,6 +86,3 @@ class App(object):
 if __name__ == '__main__':
     app = App()
     app.run(sys.argv)
-
-gwas = pd.read_table('results.txt', header=None)
-print(gwas.head(1))
